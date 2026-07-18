e3d42f063d5023557ef7d98964327cbac79771cc
arman-ali-dev <armaanali.dev@gmail.com>
2026-07-18 13:01:46 +0530

    Add two sum problem's code - medium level

---
+++ phase-2-dsa/answers/array/medium/Ans11.java
@@ -0,0 +1,21 @@
+ class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] ans = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            int first = nums[i];
+            int second = target - first;
+
+            if (map.containsKey(second)) {
+                ans[0] = map.get(second);
+                ans[1] = i;
+                break;
+            }
+
+            map.put(first, i);
+        }
+
+        return ans;
+    }
+}
